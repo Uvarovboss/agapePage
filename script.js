943506e8f10a4788345e763a930e64c802c082b3
Uvarovboss <kirilmalone1104@gmail.com>
2018-06-14 17:50:54 +0300

    second_commit

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-var img1 = document.getElementById('img1').style.color = "red";
+var images = [
+    'img/1.jpg',
+    'img/2.jpg',
+    'img/3.jpg'
+];
+var img_element = document.getElementById("img_slider");
+ var counter = 0;
+img_element.src = images[counter];
+
+function next() {
+    if (counter + 1 == images.length) {
+        counter = 0;
+    }
+    counter++;
+    img_element.src = images[counter];
+};
+
+// function prev() {
+//     if (counter - 1 == -1) {
+//         counter = images.length;
+//     }
+//     counter--;
+//     img_element.src = images[counter];
+// };
+var timerId = setInterval(function() {
+    document.getElementById('img_slider');
+    next()
+}, 2000);
